61b4489311215a36752c9b2186e4a0e6169cd660
Karsy-Kura <aksf.soccer.uverworld@gmail.com>
2023-02-24 23:40:35 +0900

    [Add][Edit] add Controllers, Models, Requests, migrations and edit Models, migrations

---
+++ app/Http/Requests/ReserveRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+
+class ReserveRequest extends FormRequest
+{
+  /**
+   * Determine if the user is authorized to make this request.
+   *
+   * @return bool
+   */
+  public function authorize()
+  {
+    return Auth::check();
+  }
+
+  /**
+   * Get the validation rules that apply to the request.
+   *
+   * @return array
+   */
+  public function rules()
+  {
+    return [
+      //
+      'date' => ['required', 'date'],
+      'time' => ['required', 'time'],
+      'num_of_people' => ['required', 'integer', 'min:1', 'max:9999'],
+    ];
+  }
+}
---
+++ app/Models/Reserve.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Reserve extends Model
+{
+  use HasFactory;
+
+  protected $fillable = [
+    'datetime',
+    'num_of_people',
+    'deleted_at',
+  ];
+
+  protected $guarded = [
+    'user_id',
+    'shop_id',
+  ];
+}
