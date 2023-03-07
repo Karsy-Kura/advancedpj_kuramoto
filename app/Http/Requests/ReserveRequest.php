86afa9119cff0727b532d029f3fb6b990bb6ffa4
Karsy-Kura <aksf.soccer.uverworld@gmail.com>
2023-03-08 03:07:00 +0900

    [Edit] change indent size 2 to 4

--- app/Http/Requests/ReserveRequest.php
+++ app/Http/Requests/ReserveRequest.php
@@ -7,28 +7,28 @@ use Illuminate\Support\Facades\Auth;
 
 class ReserveRequest extends FormRequest
 {
-  /**
-   * Determine if the user is authorized to make this request.
-   *
-   * @return bool
-   */
-  public function authorize()
-  {
-    return Auth::check();
-  }
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return Auth::check();
+    }
 
-  /**
-   * Get the validation rules that apply to the request.
-   *
-   * @return array
-   */
-  public function rules()
-  {
-    return [
-      //
-      'date' => ['required', 'date'],
-      'time' => ['required', 'time'],
-      'num_of_people' => ['required', 'integer', 'min:1', 'max:9999'],
-    ];
-  }
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            //
+            'date' => ['required', 'date'],
+            'time' => ['required', 'time'],
+            'num_of_people' => ['required', 'integer', 'min:1', 'max:9999'],
+        ];
+    }
 }
