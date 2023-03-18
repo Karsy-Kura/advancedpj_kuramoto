feb9b663707bb8d50b301885d8a35c94fe413ed1
Karsy-Kura <aksf.soccer.uverworld@gmail.com>
2023-03-18 16:07:53 +0900

    [Edit] create view (login and register)

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,59 +1,56 @@
-<x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
+<!DOCTYPE html>
+<html lang="ja">
 
-        <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rese</title>
+    <link rel="stylesheet" href="./css/app.css">
+    <link rel="stylesheet" href="./css/auth.css">
+    <link rel="stylesheet" href="./css/reset.css">
+</head>
 
-        <form method="POST" action="{{ route('register') }}">
-            @csrf
-
-            <!-- Name -->
-            <div>
-                <x-label for="name" :value="__('Name')" />
-
-                <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus />
+<body>
+    <div class="wrap">
+        <header class="header">
+            <div class="header__logo">
+                <h1 class="header__logo-text">Rese</h1>
             </div>
+        </header>
 
-            <!-- Email Address -->
-            <div class="mt-4">
-                <x-label for="email" :value="__('Email')" />
+        <section class="content">
+            <div class="auth-card">
+                <div class="auth-card__header">
+                    <h2>Registration</h2>
+                </div>
+                <div class="auth-card__input">
+                    <form action="/thanks" method="post">
+                        @csrf
+                        <div class="auth-card__input__elem">
+                            <!-- Todo: icon user -->
+                            <span class="auth-card__input__elem-logo">□</span>
+                            <input type="text" name="name" class="auth-card__input__elem-text" placeholder="Username">
+                        </div>
+                        <div class="auth-card__input__elem">
+                            <!-- Todo: icon email -->
+                            <span class="auth-card__input__elem-logo">□</span>
+                            <input type="email" name="email" class="auth-card__input__elem-text" placeholder="Email">
+                        </div>
+                        <div class="auth-card__input__elem">
+                            <!-- Todo: icon password -->
+                            <span class="auth-card__input__elem-logo">□</span>
+                            <input type="password" name="password" class="auth-card__input__elem-text" placeholder="Password">
+                        </div>
 
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
+                        <div class="auth-card__input__elem align-right">
+                            <button class="auth-card__input__button">登録</button>
+                        </div>
+                    </form>
+                </div>
             </div>
+        </section>
+    </div>
+</body>
 
-            <!-- Password -->
-            <div class="mt-4">
-                <x-label for="password" :value="__('Password')" />
-
-                <x-input id="password" class="block mt-1 w-full"
-                                type="password"
-                                name="password"
-                                required autocomplete="new-password" />
-            </div>
-
-            <!-- Confirm Password -->
-            <div class="mt-4">
-                <x-label for="password_confirmation" :value="__('Confirm Password')" />
-
-                <x-input id="password_confirmation" class="block mt-1 w-full"
-                                type="password"
-                                name="password_confirmation" required />
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
-                    {{ __('Already registered?') }}
-                </a>
-
-                <x-button class="ml-4">
-                    {{ __('Register') }}
-                </x-button>
-            </div>
-        </form>
-    </x-auth-card>
-</x-guest-layout>
+</html>
